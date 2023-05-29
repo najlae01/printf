1482c8b6c1e3ed915593470ea2905d0b0823fb70
najlae01 <najlae.abarghache1@gmail.com>
2023-05-29 16:48:51 -0700

    upadated

--- print_pointer.c
+++ print_pointer.c
@@ -2,57 +2,46 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int printf_hex(unsigned long value);
-
 /**
- * print_pointer - prints the value of a pointer
+ * print_pointer - prints a pointer address
  * @p: The pointer to print
  *
- * Return: The number of characters printed.
+ * Return: The number of characters printed
  */
-
 int print_pointer(va_list p)
 {
-	uintptr_t pointer = (uintptr_t)va_arg(p, void *);
+    void *ptr = va_arg(p, void *);
+    uintptr_t address = (uintptr_t)ptr;
+    int count = 0;
 
-	_putchar('0');
-	_putchar('x');
-	return (printf_hex((unsigned long)pointer) + 2);
+    _putchar('0');
+    _putchar('x');
+    count += 2;
+
+    if (address == 0)
+    {
+        _putchar('0');
+        count++;
+    }
+    else
+    {
+        char hexDigits[] = "0123456789abcdef";
+
+        char hexAddress[16];  // Assuming pointer size is not larger than 16 hex digits
+        int i = 0;
+
+        while (address != 0)
+        {
+            hexAddress[i++] = hexDigits[address % 16];
+            address /= 16;
+        }
+
+        for (i = i - 1; i >= 0; i--)
+        {
+            _putchar(hexAddress[i]);
+            count++;
+        }
+    }
+
+    return count;
 }
-
-/**
- * printf_hex - prints a hexadecimal value
- * @value: The value to print
- *
- * Return: The number of characters printed.
- */
-int printf_hex(unsigned long value)
-{
-	int count = 0;
-	char hex[16];
-	int i;
-	int digit;
-
-	if (value == 0)
-	{
-		_putchar('0');
-		count++;
-	}
-	else
-	{
-		while (value != 0)
-		{
-			digit = value % 16;
-			if (digit < 10)
-				hex[count] = digit + '0';
-			else
-				hex[count] = digit - 10 + 'A';
-			value /= 16;
-			count++;
-		}
-		for (i = count - 1; i >= 0; i--)
-			_putchar(hex[i]);
-	}
-	return (count);
-}
-
