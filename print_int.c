5b004aa79c2420af4179020bd4aa77324f2b5761
najlae01 <najlae.abarghache1@gmail.com>
2023-05-29 17:49:35 -0700

    upadated

--- print_int.c
+++ print_int.c
@@ -1,48 +1,55 @@
 #include "main.h"
+
 /**
-* print_int - function that prints an integer
-* @i: integer to print
-* Descriptions: prints digit with _putchar
-* Return: size the output text
-*/
+ * print_int - function that prints an integer
+ * @i: integer to print
+ *
+ * Description: prints digits with _putchar
+ * Return: size of the output text
+ */
 int print_int(va_list i)
 {
-	int len, powten, j, digit, n, count = 0, num;
+    int n = va_arg(i, int);
+    int count = 0;
+    bool flag_plus = false;
+    bool flag_space = false;
+    bool flag_hash = false;
 
-	n = va_arg(i, int);
-	if (n != 0)
-	{
-		if (n < 0)
-		{
-			_putchar('-');
-			count++;
-		}
-		num = n;
-		len = 0;
-		while (num != 0)
-		{
-			num /= 10;
-			len++;
-		}
-		powten = 1;
-		for (j = 1; j <= len - 1; j++)
-			powten *= 10;
-		for (j = 1; j <= len; j++)
-		{
-			digit = n / powten;
-			if (n < 0)
-				_putchar((digit * -1) + 48);
-			else
-				_putchar(digit + '0');
-			count++;
-			n -= digit * powten;
-			powten /= 10;
-		}
-	}
-	else
-	{
-		_putchar('0');
-		return (1);
-	}
-	return (count);
+    handle_flags(&count, flag_plus, flag_space, flag_hash);
+
+    int len, powten, j, digit, num;
+
+    if (n != 0)
+    {
+        if (n < 0)
+        {
+            _putchar('-');
+            count++;
+            n *= -1;
+        }
+        num = n;
+        len = 0;
+        while (num != 0)
+        {
+            num /= 10;
+            len++;
+        }
+        powten = 1;
+        for (j = 1; j <= len - 1; j++)
+            powten *= 10;
+        for (j = 1; j <= len; j++)
+        {
+            digit = n / powten;
+            _putchar(digit + '0');
+            count++;
+            n -= digit * powten;
+            powten /= 10;
+        }
+    }
+    else
+    {
+        _putchar('0');
+        return 1;
+    }
+    return count;
 }
